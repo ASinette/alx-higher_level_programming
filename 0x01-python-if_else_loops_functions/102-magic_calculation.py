1737f072f0b043561c0b5f21436ee5c55c1c27f3
Géradine Sinette AKOUTEY <105546591+ASinette@users.noreply.github.com>
2022-08-23 22:36:36 +0100

    Update 102-magic_calculation.py

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,13 +1,8 @@
-c_calculation(a, b, c):
-
-        if a < b:
-
-                    return c
-
-                    elif c > b:
-
-                                return a + b
-
-                                else:
-
-                                            return (a * b) - 
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return c
+    elif c > b:
+        return a + b
+    else:
+        return (a * b) - 
